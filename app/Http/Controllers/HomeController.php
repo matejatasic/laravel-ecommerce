1142850fe4c8774bbfa33714d3be2de867e72357
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-11-27 08:37:10 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::take(9)->get();
+        
+        return view('home', [
+            'products' => $products,
+        ]);
     }
 }
