d10cb1f9038be3b52c9971976a02effc59205c6f
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-11-27 10:49:19 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -29,7 +30,14 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    public function redirectTo() {
+        if(Auth::user()->isAdmin()) {
+            return '/admin/dashboard';
+        }
+        else if(Auth::user()->isUser()) {
+            return '/';
+        }
+    }
 
     /**
      * Create a new controller instance.
