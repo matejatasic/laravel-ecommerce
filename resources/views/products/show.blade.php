a42339b181ba233d867e27f12bf5bdb79f35616d
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-11-27 08:44:07 +0100

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row mb-5" id="adress-bar">
         <div class="col-md-4 text-center pt-3">
-            <p>Home > Shop > Product</p>
+            <p>Home > Shop > {{ $product->name }}</p>
         </div>
         <div class="col-md-8"></div>
     </div>
@@ -11,11 +11,11 @@
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-5">
-                <img src="{{ asset('images/microsoft_surface.png') }}" class="card-img-top" alt="product">
+                <img src="{{ asset('images/'.$product->image) }}" class="card-img-top" alt="product">
             </div>
             <div class="col-md-5">
-                <h2>Microsoft Surface</h2>
-                <p class="text-secondary">12.4" 1536 x 1024, 8GB LPDDR4x</p>
+                <h2>{{ $product->name }}</h2>
+                <p class="text-secondary">{{ $product->details }}</p>
                 <div>
                     <span>Rating:</span>&nbsp;
                     <span><i class="fas fa-star"></i></span>
@@ -24,8 +24,8 @@
                     <span><i class="fas fa-star"></i></span>
                     <span><i class="fas fa-star"></i></span>
                 </div>
-                <p id="price">350.00$</p>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p id="price">{{ $product->price }}$</p>
+                <p class="card-text">{{ $product->description }}</p>
                 <a href="#" class="btn btn-warning w-100">Add to cart</a>
             </div>
         </div>
