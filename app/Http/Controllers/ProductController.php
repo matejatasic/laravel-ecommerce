5091af5ae04d3e6fb2e8efbfb644279e9705b66e
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-11-27 08:37:39 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index() {
-        return view('products.index');
+        $products = Product::take(9)->get();
+        
+        return view('products.index', [
+            'products' => $products,
+        ]);
+    }
+
+    public function show($id) {
+        $product = Product::find($id);
+
+        return view('products.show', [
+            'product' => $product,
+        ]);
     }
 }
