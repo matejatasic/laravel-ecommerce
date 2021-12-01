988e206e3cc22a48053c62d5b59369c83d4c5cb8
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-01 09:20:28 +0100

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,6 +9,13 @@
     </div>
     <div class="container">
         <div class="row">
+            @if ($errors->any())
+                <div id="errors">
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert danger">{{$error}}</div>
+                    @endforeach
+                </div>
+            @endif
             <!-- sidebar -->
             <div class="col-md-3" id="sidebar">
                 <div class="row">
@@ -36,38 +43,69 @@
             <div class="col-md-9">
                 <h1>Shop</h1>
                 <div class="row mt-4 d-flex flex-row justify-content-between">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <a href="#"><img src="{{ asset('images/microsoft_surface.png') }}" class="card-img-top" alt="product"></a>
-                            <div class="card-body">
-                                <h5 class="card-title"><a href="#" class="text-secondary">Microsoft Surface</a></h5>
-                                <p>Price: 350$</p>
-                                <a href="#" class="btn btn-warning w-100">Add to cart</a>
+                    @foreach ($products as $product)
+                        <div class="col-md-4 mb-4">
+                            <div class="card px-2">
+                                <a href="{{ route('products.show', $product->id) }}"><img src="{{ asset('images/'.$product->image) }}" class="card-img-top" alt="product"></a>
+                                <div class="card-body">
+                                <a href="{{ route('products.show', $product->id) }}" class="product-title"><h5 class="card-title">{{ $product->name }}</h5></a>
+                                <div>
+                                    <span>Rating:</span>&nbsp;
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                </div>
+                                <p>Price: {{ $product->price }}$</p>
+                                @if (array_key_exists($product->id, session()->get('cart')))
+                                    <button class="btn btn-warning w-100" disabled>Added</button>   
+                                @else
+                                    <button class="btn btn-warning w-100 addBtn"id="{{ $product->id }}">Add to cart</button>    
+                                @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card">
-                            <a href="#"><img src="{{ asset('images/microsoft_surface.png') }}" class="card-img-top" alt="product"></a>
-                            <div class="card-body">
-                                <h5 class="card-title"><a href="#" class="text-secondary">Microsoft Surface</a></h5>
-                                <p>Price: 350$</p>
-                                <a href="#" class="btn btn-warning w-100">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card">
-                            <a href="#"><img src="{{ asset('images/microsoft_surface.png') }}" class="card-img-top" alt="product"></a>
-                            <div class="card-body">
-                                <h5 class="card-title"><a href="#" class="text-secondary">Microsoft Surface</a></h5>
-                                <p>Price: 350$</p>
-                                <a href="#" class="btn btn-warning w-100">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+        $('.addBtn').click((e) => {
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+
+            let button = $(e.target);
+            let id = button[0].id;
+            let token = "{{ csrf_token() }}";
+
+            $.ajax({
+                url: '/cart/' + id,
+                method: 'POST',
+                data: {
+                    '_token': token,
+                },
+                beforeSend() {
+                    button.prop('disabled', true);
+                    button.text('Adding...');
+                },
+                success: (res) => {
+                    button.text('Added');
+                },
+                error: (request, status, error) => {
+                    button.prop('disabled', false);
+                    button.text('Add to cart');
+                    $('#errors').html(`
+                        <div class="alert alert danger">There was an error while trying to add the item to the cart</div>    
+                    `);
+                    console.log(request.responseText);
+                }
+            });
+        })
+    </script>
 @endsection
