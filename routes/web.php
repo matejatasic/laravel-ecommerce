108bfc03c4257af7ed42ec375d843f62505f9163
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-01 10:42:11 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Auth::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::post('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/{id}', [CartController::class, 'store'])->name('cart.store');
