767f2c912f042ab0b55eba5006cf32c75c6014bb
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-02 08:52:35 +0100

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -58,7 +58,7 @@
                                     <span><i class="fas fa-star"></i></span>
                                 </div>
                                 <p>Price: {{ $product->price }}$</p>
-                                @if (array_key_exists($product->id, session()->get('cart')))
+                                @if (Session::has('cart') && array_key_exists($product->id, session()->get('cart')))
                                     <button class="btn btn-warning w-100" disabled>Added</button>   
                                 @else
                                     <button class="btn btn-warning w-100 addBtn"id="{{ $product->id }}">Add to cart</button>    
