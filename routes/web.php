3d1ea59e9b9f7850997b95749a3164c2f074ed79
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-09 15:53:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,19 @@ use App\Http\Controllers\CartController;
 
 Auth::routes();
 
+// Home
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Products
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/{id}', [CartController::class, 'store'])->name('cart.store');
-Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart.delete');
-Route::post('/cart/save/{id}', [CartController::class, 'saveForLater'])->name('cart.saveForLater');
-Route::post('/cart/add/{id}', [CartController::class, 'moveToCart'])->name('cart.moveToCart');
+
+// Cart
+Route::group(['middleware' => 'auth' ], function() {
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/{id}', [CartController::class, 'store'])->name('cart.store');
+    Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart.delete');
+    Route::post('/cart/save/{id}', [CartController::class, 'saveForLater'])->name('cart.saveForLater');
+    Route::post('/cart/add/{id}', [CartController::class, 'moveToCart'])->name('cart.moveToCart');
+});
