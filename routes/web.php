9732f04f25d6c870d70fc666258cc3d764fcaa2b
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-11 14:06:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,12 +25,17 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 
-// Cart
+
 Route::group(['middleware' => 'auth' ], function() {
+    // Cart
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
     Route::post('/cart/{id}', [CartController::class, 'store'])->name('cart.store');
     Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
     Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart.delete');
     Route::post('/cart/save/{id}', [CartController::class, 'saveForLater'])->name('cart.saveForLater');
     Route::post('/cart/add/{id}', [CartController::class, 'moveToCart'])->name('cart.moveToCart');
+
+    // Checkout
+    Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
+    Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
 });
