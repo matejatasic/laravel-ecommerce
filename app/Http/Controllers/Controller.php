5bfc00542a6681fc72d15186e493027354265f70
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-13 10:59:58 +0100

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getPrice($cart) {
+        $subtotal = 0;
+        
+        foreach($cart as $cartProduct) {
+            $subtotal += $cartProduct->product->price * $cartProduct->quantity;
+        }
+
+        return [
+            'subtotal' => $subtotal,
+            'tax' => $subtotal / 10
+        ];
+    }
 }
