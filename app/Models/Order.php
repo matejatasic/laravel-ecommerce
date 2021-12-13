4406086b8a79cd6b6e27903829e5485b200fc6f6
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-13 12:14:18 +0100

    Add files via upload

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Product;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'email', 'name', 'address', 'city', 'province', 'postalcode', 'phone', 'name_on_card', 'subtotal', ''
+    ];
+
+    public function user() {
+        $this->belongsTo(User::class);
+    }
+
+    public function products() {
+        $this->belongsToMany(Product::class);
+    }
+}
