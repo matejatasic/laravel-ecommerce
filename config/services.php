41a7dc0b8f96529bd14a3d4ef3ec94619afbcaf3
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-15 09:52:08 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -29,5 +29,8 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    
+    'stripe' => [
+        'secret' => 'you stripe key here',
+    ]
 ];
