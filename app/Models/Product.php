eff7e22698f1c5757a3a43867e626382830a80b8
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-17 10:11:04 +0100

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'image', 'slug', 'details', 'description', 'price', 'featured', 'quantity', 'category_id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
