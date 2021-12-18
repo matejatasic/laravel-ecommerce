d8df16ee0cf1188493366c7ecdc136c75ae185c4
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-18 13:40:24 +0100

    Add files via upload

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">Dashboard</h1>
+            <hr>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-4">
+            <div class="card text-white bg-info mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-white bg-info mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-white bg-info mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
