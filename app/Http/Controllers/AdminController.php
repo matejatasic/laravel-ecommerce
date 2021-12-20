05b4bda653b6a6bc826c7cdeb88b7301775494c6
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-20 10:18:26 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
     public function dashboard() {
-        return view('admin.dashboard');
+        $product_count = Product::where('quantity', '>', 0)->count();
+        $order_count = Order::where('error', null)->count();
+        $customer_count = count(Order::select('user_id')->distinct()->get());
+
+        return view('admin.dashboard', [
+            'product_count' => $product_count,
+            'order_count' => $order_count,
+            'customer_count' => $customer_count,
+        ]);
     }
 }
