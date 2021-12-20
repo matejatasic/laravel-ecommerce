0ce6de4cc333ad5b5fed765e97fb68f3c5673075
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-12-20 10:17:59 +0100

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,29 +9,26 @@
     </div>
     <div class="row mt-3">
         <div class="col-md-4">
-            <div class="card text-white bg-info mb-3">
-                <div class="card-header">Header</div>
+            <div class="card text-white bg-info mb-3 text-center">
+                <div class="card-header">Products</div>
                 <div class="card-body">
-                    <h5 class="card-title">Info card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h2 class="card-title">{{ $product_count }}</h2>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card text-white bg-info mb-3">
-                <div class="card-header">Header</div>
+            <div class="card text-white bg-info mb-3 text-center">
+                <div class="card-header">Orders</div>
                 <div class="card-body">
-                    <h5 class="card-title">Info card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h2 class="card-title">{{ $order_count }}</h2>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card text-white bg-info mb-3">
-                <div class="card-header">Header</div>
+            <div class="card text-white bg-info mb-3 text-center">
+                <div class="card-header">Customers</div>
                 <div class="card-body">
-                    <h5 class="card-title">Info card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h2 class="card-title">{{ $customer_count }}</h2>
                 </div>
             </div>
         </div>
