260d62de44f1aeee0350c1602b3f8d023241fc1f
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2022-01-05 15:16:04 +0100

    Update orders.blade.php

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -2,43 +2,45 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 col-sm-12 col-12">
             <h1 class="text-center">Orders</h1>
             <hr>
         </div>
     </div>
     <div class="row mt-3">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Email</th>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th>Total Price</th>
-                    <th>Shipped</th>
-                    <th>Date</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($orders as $order)
+        <div class="col-md-12 col-sm-12 col-12">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $order->id }}</td>
-                        <td>{{ $order->email }}</td>
-                        <td>{{ $order->name }}</td>
-                        <td>{{ $order->phone }}</td>
-                        <td>{{ $order->total }}$</td>
-                        <td>{{ $order->shipped !== 0 ? 'Yes' : 'No' }}</td>
-                        <td>{{ date('j F, Y', strtotime($order->created_at)) }}</td>
-                        <td>
-                            <button class="btn btn-primary viewBtn" id="{{ $order->id }}">View</button>
-                        </td>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Total Price</th>
+                        <th>Shipped</th>
+                        <th>Date</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $orders->links() }}
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <td>{{ $order->id }}</td>
+                            <td>{{ $order->email }}</td>
+                            <td>{{ $order->name }}</td>
+                            <td>{{ $order->phone }}</td>
+                            <td>{{ $order->total }}$</td>
+                            <td>{{ $order->shipped !== 0 ? 'Yes' : 'No' }}</td>
+                            <td>{{ date('j F, Y', strtotime($order->created_at)) }}</td>
+                            <td>
+                                <button class="btn btn-primary viewBtn" id="{{ $order->id }}">View</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $orders->links() }}
+        </div>
     </div>
 
     <!-- modal -->
