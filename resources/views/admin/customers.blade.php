55b66a651319c3d423299168b1e6842a93624927
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2022-01-05 16:09:57 +0100

    Update customers.blade.php

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -2,36 +2,38 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 col-sm-12 col-12">
             <h1 class="text-center">Costumers</h1>
             <hr>
         </div>
     </div>
     <div class="row mt-3">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Email</th>
-                    <th>Name</th>
-                    <th>Role</th>
-                    <th>Orders</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($customers as $customer)
+        <div class="col-md-12 col-sm-12 col-12">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $customer->id }}</td>
-                        <td>{{ $customer->email }}</td>
-                        <td>{{ $customer->name }}</td>
-                        <td>{{ ucfirst($customer->role) }}</td>
-                        <td>{{ count($customer->orders->where('error', null)) }}</td>
-                        <td>{{ date('j F, Y', strtotime($customer->created_at)) }}</td>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Role</th>
+                        <th>Orders</th>
+                        <th>Date</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $customers->links() }}
+                </thead>
+                <tbody>
+                    @foreach ($customers as $customer)
+                        <tr>
+                            <td>{{ $customer->id }}</td>
+                            <td>{{ $customer->email }}</td>
+                            <td>{{ $customer->name }}</td>
+                            <td>{{ ucfirst($customer->role) }}</td>
+                            <td>{{ count($customer->orders->where('error', null)) }}</td>
+                            <td>{{ date('j F, Y', strtotime($customer->created_at)) }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $customers->links() }}
+        </div>
     </div>
 @endsection
